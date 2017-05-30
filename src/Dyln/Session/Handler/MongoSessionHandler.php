9c4ba01f661a58feeecde9b15f39260c98099482
bill <bill@dyln.io>
2017-05-30 10:19:01 +0100

    Update account user

--- src/Dyln/Session/Handler/MongoSessionHandler.php
+++ src/Dyln/Session/Handler/MongoSessionHandler.php
@@ -21,7 +21,11 @@ class MongoSessionHandler implements \SessionHandlerInterface
     protected function __construct($host, $databaseName, $collectionName, array $config)
     {
         $this->sessionConfig = ArrayUtil::getIn($config, ['session_config'], []);
-        $manager = new Manager($host, [], []);
+        $manager = new Manager($host, [
+            'connectTimeoutMS'         => 30000,
+            'serverSelectionTimeoutMS' => 60000,
+            'socketTimeoutMS'          => 60000,
+        ], []);
         $db = new Database($manager, $databaseName, [
             'typeMap' => [
                 'array'    => 'array',
