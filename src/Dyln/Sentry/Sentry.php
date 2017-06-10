f73950f69d439b70d1d9c5ec7d32615957d4cfe4
bill <bill@dyln.io>
2017-06-10 15:15:35 +0100

    basket delivery

---
+++ src/Dyln/Sentry/Sentry.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Dyln\Sentry;
+
+use Dyln\AppEnv;
+
+class Sentry
+{
+    static protected $instance;
+    /** @var  \Raven_Client */
+    protected $client;
+
+    private function __construct(\Raven_Client $client)
+    {
+        $this->client = $client;
+    }
+
+    static public function addExtraContext($key, $value)
+    {
+        self::getInstance()->client->extra_context([
+            $key => $value,
+        ]);
+    }
+
+    static public function getInstance()
+    {
+        if (!self::$instance) {
+            $client = new \Raven_Client(AppEnv::env('sentry.url'));
+            self::$instance = new self($client);
+        }
+
+        return self::$instance;
+
+    }
+
+    static public function info($message, $params = [], $data = [], $stack = true, $vars = null, $tags = [])
+    {
+        $data['level'] = \Raven_Client::INFO;
+        self::getInstance()->message($message, $params, $data, $stack, $vars, $tags);
+    }
+
+    static public function message($message, $params = [], $data = [], $stack = true, $vars = null, $tags = [])
+    {
+        array_walk($params, function (&$value) {
+            $value = (string)$value;
+        });
+        $data['tags'] = $tags;
+        self::getInstance()->client->captureMessage($message, $params, $data, $stack, $vars);
+    }
+
+    static public function debug($message, $params = [], $data = [], $stack = true, $vars = null, $tags = [])
+    {
+        $data['level'] = \Raven_Client::DEBUG;
+        self::getInstance()->message($message, $params, $data, $stack, $vars, $tags);
+    }
+
+    static public function error($message, $params = [], $data = [], $stack = true, $vars = null, $tags = [])
+    {
+        $data['level'] = \Raven_Client::ERROR;
+        self::getInstance()->message($message, $params, $data, $stack, $vars, $tags);
+    }
+
+    static public function fatal($message, $params = [], $data = [], $stack = true, $vars = null, $tags = [])
+    {
+        $data['level'] = \Raven_Client::FATAL;
+        self::getInstance()->message($message, $params, $data, $stack, $vars, $tags);
+    }
+
+    static public function exception($e)
+    {
+        self::getInstance()->client->captureException($e);
+    }
+
+    public function register($version)
+    {
+        global $argv;
+        $client = self::getInstance()->client;
+        $client->setRelease($version);
+        $client->setEnvironment(AppEnv::getAppEnv());
+        $client->extra_context([
+            'argv'   => $argv ? implode(' ', $argv) : null,
+            'env'    => $_ENV,
+            'server' => $_SERVER,
+        ]);
+        if (php_sapi_name() == 'cli') {
+            $client->tags_context(['browser' => 'cli']);
+        }
+//        if (AppEnv::isLive()) {
+        $client->install();
+//        }
+    }
+}
