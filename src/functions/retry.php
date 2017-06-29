ce1c3e2d3a2dae943aa5a71202a1cccf4123c96e
bill <bill@dyln.io>
2017-06-29 12:33:05 +0100

    retry function

--- src/functions/retry.php
+++ src/functions/retry.php
@@ -2,14 +2,14 @@
 
 namespace Dyln;
 
-function _retry(callable $callable, $maxTry = 5)
+function _retry(callable $callable, $maxTry = 5, $sleep = 0.5)
 {
     while ($maxTry) {
         try {
             return $callable();
             break;
         } catch (\Exception $e) {
-            sleep(0.5);
+            sleep($sleep);
             $maxTry--;
             if (!$maxTry) {
                 throw $e;
