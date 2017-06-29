9d202e28288d8c0294867cf4128d003012b8c53a
bill <bill@dyln.io>
2017-06-29 10:04:18 +0100

    Move env to functions

---
+++ src/functions/env.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!function_exists('_load_env_')) {
+    function _load_env_($dir)
+    {
+        $autodetect = ini_get('auto_detect_line_endings');
+        ini_set('auto_detect_line_endings', '1');
+        $lines = file($dir, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        ini_set('auto_detect_line_endings', $autodetect);
+
+        foreach ($lines as $line) {
+            $line = trim($line);
+            if ($line) {
+                list($name, $value) = explode('=', $line);
+                $name = trim($name);
+                $value = trim($value);
+                if (function_exists('apache_getenv') && function_exists('apache_setenv') && apache_getenv($name)) {
+                    apache_setenv($name, $value);
+                }
+
+                if (function_exists('putenv')) {
+                    putenv("$name=$value");
+                }
+
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
+}
