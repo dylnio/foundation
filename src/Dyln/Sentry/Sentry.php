0bbc2b77f90ac10a194eed7e5135f0f7f1578a41
bill <bill@dyln.io>
2017-07-24 07:52:04 +0100

    NewRelic transaction name middleware

--- src/Dyln/Sentry/Sentry.php
+++ src/Dyln/Sentry/Sentry.php
@@ -79,6 +79,9 @@ class Sentry
         if ($e instanceof NotLoggableException) {
             return;
         }
+        if (extension_loaded('newrelic')) {
+            newrelic_notice_error($e->getMessage(), $e);
+        }
         if (self::getInstance()->enabled) {
             self::getInstance()->client->captureException($e);
         }
