9e142d89e1716ffb5b4152902ec6a0fe5711b7c6
bill <bill@dyln.io>
2017-07-27 10:15:09 +0100

    options apc

--- src/Dyln/Slim/ModuleConfigSerializer.php
+++ src/Dyln/Slim/ModuleConfigSerializer.php
@@ -19,9 +19,9 @@ class ModuleConfigSerializer
             $bits = explode('\\', $moduleClass);
             $dir = ROOT_DIR . '/app/src/Modules/' . $bits[2];
             $configFile = $dir . '/_config.php';
-            if (file_exists($configFile)) {
-                /** @noinspection PhpIncludeInspection */
-                $config = @include $configFile;
+            /** @noinspection PhpIncludeInspection */
+            $config = @include $configFile;
+            if ($config) {
                 $moduleServices = ArrayUtil::getIn($config, ['services'], []);
                 $moduleParams = ArrayUtil::getIn($config, ['params'], []);
                 $services = array_merge($services, $moduleServices);
