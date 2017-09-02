017dcc839546007aec27e139f43e16f11c9fd798
Bill Yildirim <bill@dyln.io>
2017-09-02 21:59:53 +0100

    use display_name instead of displayname

--- src/Dyln/Firewall/Identity/Identity.php
+++ src/Dyln/Firewall/Identity/Identity.php
@@ -22,7 +22,7 @@ class Identity implements IdentityInterface
 
     public function getDisplayName()
     {
-        return $this->data['displayname'];
+        return $this->data['display_name'];
     }
 
     public function isLoggedIn()
