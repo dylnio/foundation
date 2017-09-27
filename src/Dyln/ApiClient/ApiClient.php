ce4bc724bbef945d142be68d7c02c6aebbe88890
bill <bill@dyln.io>
2017-09-27 15:22:53 +0100

    Inject tracking to api request

--- src/Dyln/ApiClient/ApiClient.php
+++ src/Dyln/ApiClient/ApiClient.php
@@ -2,6 +2,7 @@
 
 namespace Dyln\ApiClient;
 
+use App\Lib\Slim\Middleware\ParseTrackingUrlParamsMiddleware;
 use Dyln\ApiClient\ResponseBodyMiddleware\ConvertToMessageMiddleware;
 use Dyln\ApiClient\ResponseBodyMiddleware\DebugbarMiddleware;
 use Dyln\ApiClient\ResponseBodyMiddleware\JsonDecodeMiddleware;
@@ -56,6 +57,7 @@ class ApiClient
 
     public function call($path, array $query = null, array $data = null, $method = 'GET', $options = []): Message
     {
+        $query = $this->injectTracking($query);
         $this->addResponseBodyMiddleware(new ConvertToMessageMiddleware());
         if (AppEnv::isDebugEnabled()) {
             $query['XDEBUG_SESSION_START'] = 'PHPSTORM';
@@ -77,6 +79,7 @@ class ApiClient
         if ($data) {
             $requestOptions['body'] = json_encode($data);
         }
+
         try {
             $res = $this->request($method, $path, $requestOptions);
             $responseHeaders = $res->getHeaders();
@@ -202,5 +205,16 @@ class ApiClient
         }
 
         return $response;
+    }
+
+    private function injectTracking($query = null)
+    {
+        if (!$query) {
+            $query = [];
+        }
+        $query['trk_scid'] = $_COOKIE[ParseTrackingUrlParamsMiddleware::SCID_COOKE_NAME] ?? null;
+        $query['trk_iid'] = $_COOKIE[ParseTrackingUrlParamsMiddleware::IID_COOKE_NAME] ?? null;
+
+        return $query;
     }
 }
