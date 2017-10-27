3fb869f55d61136012b6c1ebda6da5b029359a4f
Bill Yildirim <bill@dyln.io>
2017-10-27 15:14:39 +0100

    https://sentry.io/shopcade/live-marketplace-api/issues/393583432/

--- src/Dyln/Sentry/Middleware/RequestInfoMiddleware.php
+++ src/Dyln/Sentry/Middleware/RequestInfoMiddleware.php
@@ -11,7 +11,7 @@ class RequestInfoMiddleware
     public function __invoke(Request $request, Response $response, $next)
     {
         if (php_sapi_name() !== 'cli') {
-            Sentry::addExtraContext('_url_', $_SERVER['REQUEST_URI']);
+            Sentry::addExtraContext('_path_', $request->getUri()->getPath());
             Sentry::addExtraContext('_query_', json_encode($request->getQueryParams()));
             $body = (string) $request->getBody();
             Sentry::addExtraContext('_body_', $body);
