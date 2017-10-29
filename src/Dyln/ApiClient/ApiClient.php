7d9d3e49cbef979ec4599ac2380ba5e393d10137
Bill Yildirim <bill@dyln.io>
2017-10-29 16:25:41 +0000

    Page's now have type and overwrite_template field. Also getting page by criteria is deprecated.

--- src/Dyln/ApiClient/ApiClient.php
+++ src/Dyln/ApiClient/ApiClient.php
@@ -61,8 +61,8 @@ class ApiClient
         $this->userToken = getin($options, 'user.token');
     }
 
-    public function call($path, array $query = null, array $data = null, $method = 'GET', $options = [])
-    : Message {
+    public function call($path, array $query = null, array $data = null, $method = 'GET', $options = []) : Message
+    {
         if (!$query) {
             $query = [];
         }
